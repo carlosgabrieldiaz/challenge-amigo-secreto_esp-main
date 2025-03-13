6837b2f2c068868f3d0903a00a1adda53d6ba275
Carlos <carlosgabrieldiaz@gmail.com>
2025-03-13 14:00:10 -0300

    Challeng Aigo secreto UPDATED

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+const inputAmigo = document.getElementById("amigo");
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById("listaAmigos");
+const ulResultado = document.getElementById("resultado");
+
+
+function agregarAmigo () {
+    if (inputAmigo.value == ""){
+        alert("Por favor, inserte un nombre.")
+        return;
+    }
+
+    listaAmigos.push(inputAmigo.value);
+    
+      
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+    
+    inputAmigo.value = "";
+
+    };
+
+
+
+function  sortearAmigo() {
+    const random = Math.floor(Math.random() *listaAmigos.length);
+    const amigoSecreto = listaAmigos [random]
+    ulResultado.innerHTML = `<li>El amigo secreto es: ${amigoSecreto}</li>`;
+
+
+    }
+
+    
+ 
+
+
+
+//variables
+//arreglos
+//funciones
+//eventos
+// //objetos
+
